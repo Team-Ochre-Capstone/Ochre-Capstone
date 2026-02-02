25725a6f747f2b92c0b03442d50ef19be6dc9bca
Israk <israkayasin@gmail.com>
2026-02-02 15:59:26 -0500

    removed background options on settings page

--- src/webapp/src/pages/SettingsPage.tsx
+++ src/webapp/src/pages/SettingsPage.tsx
@@ -86,8 +86,6 @@ const SettingsPage = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none"
               >
                 <option value="white">White (default)</option>
-                <option value="light-gray">Light Gray (default)</option>
-                <option value="black">Black</option>
               </select>
             </div>
 
